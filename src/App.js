61da4241e6a582475c0765b98b2e8a967ec1afe8
ana-barreira <74630055+ana-barreira@users.noreply.github.com>
2021-03-22 23:25:37 +0000

    Added Credits component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from "react";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import WeatherSearch from "./WeatherSearch";
+import Credits from "./Credits";
 
 export default function App() {
   return (
+    <div className="container">
     <div className="App">
       <p/> <p/> <br />
       <WeatherSearch defaultCity= "Braga"/>
-      
-  <p className="credits">
-  <a className="footer" href="https://github.com/ana-barreira/react-weather-app" target="_blank" rel="noreferrer" >Open-source code </a> by <a href="https://www.linkedin.com/in/ana-barreira-7529b3183/" target="_blank" rel="noreferrer">Ana Barreira </a>
- </p>  <p/> <p/> <br />
     </div>
-  );}
+  
+  <Credits />
+  </div>);
+}
+
+ 
 
---
+++ src/Credits.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Credits(){
+return ( 
+<div className="credits">
+  <p />
+  This project was coded by <a href="https://www.linkedin.com/in/ana-barreira-7529b3183/" target="_blank" rel="noreferrer">Ana Barreira </a> and is <a className="footer" href="https://github.com/ana-barreira/react-weather-app" target="_blank" rel="noreferrer" > open-source on GitHub</a> and <a href="https://kind-tereshkova-eeb54e.netlify.app/">hosted on Netlify</a>
+  <p/> <p/> <br />
+ </div>);
+}
