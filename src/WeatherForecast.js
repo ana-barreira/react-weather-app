6dc08c285c2907f15858dd72b4d9d710f9118478
ana-barreira <74630055+ana-barreira@users.noreply.github.com>
2021-03-30 18:36:09 +0100

    Added weather forecast map

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -15,13 +15,17 @@ export default function WeatherForecast(props){
 if (loaded && props.city === forecast.city.name){
   return (
     <div className="WeatherForecast row">
-     
-        <WeatherForecastPreview data={forecast.list[0]} />
-    <WeatherForecastPreview data={forecast.list[1]} />
-    <WeatherForecastPreview data={forecast.list[2]} />
-    <WeatherForecastPreview data={forecast.list[3]} />
-    <WeatherForecastPreview data={forecast.list[4]} />
-    </div>) ;
+      {forecast.list.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="WeatherForecastPreview col" key={index}>
+                  <WeatherForecastPreview data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
+        </div>
+    );
 } else{
 let apiKey=`377b65e5ab39dbbbf56ae5019ed7e717`;
 let url=`https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=${apiKey}&units=metric`;
