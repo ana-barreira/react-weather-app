f89d3c630a03773af39423e42a97dd6f97308793
ana-barreira <74630055+ana-barreira@users.noreply.github.com>
2021-04-01 17:38:17 +0100

    Updated credits link response

--- src/Credits.js
+++ src/Credits.js
@@ -4,7 +4,7 @@ export default function Credits(){
 return ( 
 <div className="credits">
   <p /> <br />
-  This project was coded by <a href="https://www.linkedin.com/in/ana-barreira-7529b3183/" target="_blank" rel="noreferrer">Ana Barreira </a> and is <a className="footer" href="https://github.com/ana-barreira/react-weather-app" target="_blank" rel="noreferrer" > open-source on GitHub</a> and <a href="https://kind-tereshkova-eeb54e.netlify.app/">hosted on Netlify</a>
+  This project was coded by <a href="https://www.linkedin.com/in/ana-barreira-7529b3183/" target="_blank" rel="noreferrer">Ana Barreira </a> and is <a className="footer" href="https://github.com/ana-barreira/react-weather-app" target="_blank" rel="noreferrer" > open-source on GitHub</a> and <a href="https://kind-tereshkova-eeb54e.netlify.app/" target="_blank" rel="noreferrer">hosted on Netlify</a>
   <p/> <p/> <br />
  </div>);
 }
